e9c836d4b8158094a851ffe32cbd917121a29b41
hollowbit <nathanael.maher@outlook.com>
2024-01-12 15:44:48 -0500

    Fixed unintentional compile bug

--- lib/joinmy_party_web.ex
+++ lib/joinmy_party_web.ex
@@ -91,7 +91,6 @@ defmodule JoinmyPartyWeb do
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
-      import JoinmyPartyWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
